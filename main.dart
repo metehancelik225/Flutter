d5c117414044885294e1990e077e75ab906eef05
Metehan Çelik <84625267+metehancelik225@users.noreply.github.com>
2021-12-03 21:06:31 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(
+    Text(
+      "Merhaba",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.lightGreenAccent,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
